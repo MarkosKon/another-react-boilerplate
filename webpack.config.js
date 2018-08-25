098735925ccd04b10d2b26d15917dded0f129e78
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2018-08-25 13:03:52 +0300

    Initial project set up

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack"); //to access built-in plugins
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "react"]
+          }
+        }
+      },
+      { test: /\.css$/, use: "css-loader", exclude: /(node_modules)/ }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({ template: "./src/index.html" })
+  ]
+};
