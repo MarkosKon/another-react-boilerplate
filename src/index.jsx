2e8a2edbc203edb4a98bc2a29fbf6ef64c452829
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2018-08-29 00:51:32 +0300

    Register service worker only in production.

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('app'));
 
 // Check that service workers are registered
-if ('serviceWorker' in navigator) {
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js');
