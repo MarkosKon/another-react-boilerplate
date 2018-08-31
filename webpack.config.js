09b7976fd5a38496c39e0e5638d452577325666f
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2018-08-31 11:20:57 +0300

    Added file loader for importing images.

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,15 @@ module.exports = {
         },
       },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
     ],
   },
   plugins: [
