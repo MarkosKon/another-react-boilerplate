f79d5c43e276545312670fd612ea2bc41dae4df9
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2018-09-02 20:43:34 +0300

    Added 'dotenv' package for enviromental variable support.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack'); // to access built-in plugins
+const dotenv = require('dotenv');
 const path = require('path');
+
+// call dotenv and it will return an Object with a parsed key 
+const env = dotenv.config().parsed;
+  
+// reduce it to a nice object, the same as before
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   entry: './src/index.jsx',
@@ -44,6 +54,7 @@ module.exports = {
   },
   plugins: [
     new webpack.ProgressPlugin(),
+    new webpack.DefinePlugin(envKeys),
     new HtmlWebpackPlugin({ template: './src/index.html' }),
     new CopyWebpackPlugin([{ from: 'public' }]),
     new WorkboxPlugin.GenerateSW(),
